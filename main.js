8f4fb1f472f6a443a94f0552a5fe35ff8b63ed32
axelaahlin <axel.ahlinandersson@yh.nackademin.se>
2021-10-15 11:20:51 +0200

    Spara

--- main.js
+++ main.js
@@ -38,4 +38,28 @@ fastTrack.addEventListener ("click", () => {
 kön.unshift(input.value.toLowerCase());
 console.log(kön)
 
-})
+if (input.value === "") {
+  alert("Vänligen skriv in ett namn.")
+} else {
+  let listItem = document.createElement("li");
+  let listItemText = document.createElement("span");
+  let checkainbutton = document.createElement("button");
+  
+  checkainbutton.innerText = "Incheckad";
+  listItemText.innerText = input.value;
+  
+  checkainbutton.addEventListener("click", (e) => {
+  listItem.remove(); 
+  });
+  //Eventlistener som tar bort från listan.
+  
+  lista.prepend(listItem); 
+  listItem.appendChild(listItemText);
+  listItem.appendChild(checkainbutton);
+  //Appendar alla element.
+}
+input.value = "";
+
+
+
+});
